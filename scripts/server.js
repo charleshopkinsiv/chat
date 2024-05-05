747c37efc61f003f045d9be4d815e58c22ce0da2
Charles Hopkins <charleshopkinsiv@gmail.com>
2024-05-04 21:58:23 -0700

    Completed functionality on server code

--- scripts/server.js
+++ scripts/server.js
@@ -2,11 +2,41 @@ import WebSocket, { WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 8080 });
 
+let users = [];
+
 wss.on('connection', function connection(ws) {
+    
+    // Add user to array and broadcast
+    users.push(ws);
+    broadcastMessage({
+        type: 'newUser',
+        username: ws.username
+    });
+
     ws.on('message', function incoming(message) {
+        const type = message.type;
+        const to = message.to;
         message = message.toString();
-        broadcastMessage(message);
-        console.log('received: %s', message, typeof message);
+
+        // Send message to a single user
+        if(type === 'direct') {
+            users.forEach(function each(client) {
+                if (client.username === to) {
+                && client.readyState === WebSocket.OPEN) {
+                    client.send(message);
+                }
+            });
+        }
+
+        // Broadcast message to all users
+        elseif(type === 'chat') {
+            broadcastMessage(message);
+        }
+
+        // Remove user from array and broadcast
+        elseif(type === 'logout') {
+            users.splice(users.indexOf(ws), 1);
+        }
     });
 });
 
